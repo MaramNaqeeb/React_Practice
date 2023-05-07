cdeb0d14d8692d89d2fbae50db82271c5fe5d35d
MaramNaqeeb <maramnaqib1998@gmail.com>
2023-05-07 14:07:25 +0300

    accordion

---
+++ Accordion_complete/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import data from "./data"
+import SingleQuestion from "./Question"
+import React, { useState } from "react";
+
+function App() {
+  const[questions,setQuestions]=useState(data)
+  return (
+    <main >
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+{
+  questions.map((question)=>{
+return <SingleQuestion key={question.id} {...question}/>
+  })
+}
+        </section>
+      </div>
+    </main>
+  );
+}
+export default App;
