1ce3fa12b76723a8f52918c68118dffaba59264c
MaramNaqeeb <maramnaqib1998@gmail.com>
2023-05-07 16:51:50 +0300

    menu

---
+++ Menu/src/data.js
@@ -0,0 +1,83 @@
+const menu = [
+  {
+    id: 1,
+    title: "buttermilk pancakes",
+    category: "breakfast",
+    price: 15.99,
+    img: "https://simplyhomecooked.com/wp-content/uploads/2022/08/buttermilk-pancakes-2.jpg",
+    desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+  },
+  {
+    id: 2,
+    title: "diner double",
+    category: "lunch",
+    price: 13.99,
+    img: "https://cdn.metrodiner.io/wp-content/uploads/cdn.filestackcontent.com/1sH1RuG6QHeeeUMFg3Hk/medium",
+
+    desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+  },
+  {
+    id: 3,
+    title: "godzilla milkshake",
+    category: "shakes",
+    price: 6.99,
+    img: "https://www.siftandsimmer.com/wp-content/uploads/2022/11/milo-dinosaur2.jpg",
+
+    desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+  },
+  {
+    id: 4,
+    title: "country delight",
+    category: "breakfast",
+    price: 20.99,
+    img: "https://assets.entrepreneur.com/content/3x2/2000/1606136871-breakfast.jpg",
+
+    desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+  },
+  {
+    id: 5,
+    title: "egg attack",
+    category: "lunch",
+    price: 22.99,
+    img: "https://i.pinimg.com/474x/d2/08/85/d20885ca30e3500d51f89722154183c9.jpg",
+
+    desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+  },
+  {
+    id: 6,
+    title: "oreo dream",
+    category: "shakes",
+    price: 18.99,
+    img: "https://cdn-02.bilbayt.com/menu-images/oreo-dream-shake-w480-638067002419388215.jpg",
+
+    desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+  },
+  {
+    id: 7,
+    title: "bacon overflow",
+    category: "breakfast",
+    price: 8.99,
+    img: "https://healthyrecipesblogs.com/wp-content/uploads/2022/08/bacon-egg-cups-1-2022.jpg",
+
+    desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+  },
+  {
+    id: 8,
+    title: "american classic",
+    category: "lunch",
+    price: 12.99,
+    img: "https://images.yen.com.gh/images/d357b7d5a78af496.jpg",
+
+    desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+  },
+  {
+    id: 9,
+    title: "quarantine buddy",
+    category: "shakes",
+    price: 16.99,
+    img: "https://pbs.twimg.com/media/EVhDJHHWoAAEbkB.jpg:large",
+
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
+];
+export default menu;
